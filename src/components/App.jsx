bbdd6ffb3fc462cac12db3c1633e8067dc176cc5
BiancaMesa <bianca.koraa@gmail.com>
2024-03-18 14:22:29 +0100

    preview component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./Header";
+import Preview from "./Preview";
 import Button from "./Button";
 import Footer from "./Footer";
 import '../scss/App.scss';
@@ -67,38 +68,17 @@ function App() {
           <a className="button--link" href="./">Ver proyectos</a>
         </section>
 
-        <section className="preview">
-          <div className="projectImage"></div>
-          <article className="card">
-            <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal Project Card</span></h2>
+       <Preview 
+          userJob={userJob} 
+          userName={userName} 
+          projectName={projectName} 
+          slogan={slogan} 
+          descriptions={descriptions} 
+          usedTechs={usedTechs} 
+          repoLink={repoLink} 
+          demoLink={demoLink}
+        />
 
-            <div className="card__author">
-              <div className="card__authorPhoto"></div>
-              <p className="card__job">
-                {userJob}
-              </p>
-              <h3 className="card__name">{userName}</h3>
-            </div>
-
-            <div className="card__project">
-              <h3 className="card__name">{projectName}</h3>
-              <p className="card__slogan">{slogan}</p>
-              <h3 className="card__descriptionTitle">Product description</h3>
-              <p className="card__description">{descriptions}</p>
-
-              <div className="card__technicalInfo">
-                <p className="card__technologies">{usedTechs}</p>
-
-                <a className="icon icon__www" href={repoLink} target="_blank" title="Haz click para ver el proyecto online">
-                  Web link
-                </a>
-                <a className="icon icon__github" href={demoLink} target="_blank" title="Haz click para ver el código del proyecto">
-                  GitHub link
-                </a>
-              </div>
-            </div>
-          </article>
-        </section>
         <form className="addForm">
           <h2 className="title">Información</h2>
           <fieldset className="addForm__group">
---
+++ src/components/Preview.jsx
@@ -0,0 +1,78 @@
+import "../scss/components/Preview.scss";
+import PropTypes from "prop-types";
+// import ebookImg from '../images/ebook-example.jpg';
+// import avatar from '../images/avatar.webp';
+// import gitHubIcon from '../images/github.svg';
+// import globeIcon from '../images/globe-solid.svg';
+
+function Preview({
+  userJob,
+  userName,
+  projectName,
+  slogan,
+  descriptions,
+  usedTechs,
+  repoLink,
+  demoLink,
+}) {
+
+  return (
+    <section className="preview">
+      <div className="projectImage"></div>
+      <article className="card">
+        <h2 className="card__projectTitle">
+          <span className="card__projectTitle--text">
+            Personal Project Card
+          </span>
+        </h2>
+
+        <div className="card__author">
+          <div className="card__authorPhoto"></div>
+          <p className="card__job">{userJob}</p>
+          <h3 className="card__name">{userName}</h3>
+        </div>
+
+        <div className="card__project">
+          <h3 className="card__name">{projectName}</h3>
+          <p className="card__slogan">{slogan}</p>
+          <h3 className="card__descriptionTitle">Product description</h3>
+          <p className="card__description">{descriptions}</p>
+
+          <div className="card__technicalInfo">
+            <p className="card__technologies">{usedTechs}</p>
+
+            <a
+              className="icon icon__www"
+              href={repoLink}
+              target="_blank"
+              title="Haz click para ver el proyecto online"
+            >
+              Web link
+            </a>
+            <a
+              className="icon icon__github"
+              href={demoLink}
+              target="_blank"
+              title="Haz click para ver el código del proyecto"
+            >
+              GitHub link
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+Preview.propTypes = {
+  userJob: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  projectName: PropTypes.string.isRequired,
+  slogan: PropTypes.string.isRequired,
+  descriptions: PropTypes.string.isRequired,
+  usedTechs: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
+  demoLink: PropTypes.string.isRequired,
+};
+
+export default Preview;
