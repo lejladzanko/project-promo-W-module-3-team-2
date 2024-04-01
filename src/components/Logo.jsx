c4d31a5def4a62b0cb08d48be3352b9f6edcf52a
BiancaMesa <bianca.koraa@gmail.com>
2024-04-01 21:54:32 +0200

    route added to log

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
+import {Link} from "react-router-dom";
 import logoAdalab from "../images/adalab.png";
 import "../scss/components/Logo.scss";
 
 function Logo() {
   return (
-    <a href="/">
+    <Link to="/">
       <img className="logoSponsor" src={logoAdalab} alt="Logo Adalab" />
-    </a>
+    </Link>
   );
 }
 
